4ac102092073456ce93a5d010bf9fe7f1e79b2c6
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-10-12 08:04:24 -0400

    Add the starting versions of all provided Java source code.

---
+++ src/main/java/practicalfive/repeat/AppendRepeater.java
@@ -0,0 +1,23 @@
+package practicalfive.repeat;
+
+/**
+ * A class that constructs a string of a repeated character with appends.
+ *
+ * @author Gregory M. Kapfhammer
+ */
+
+public class AppendRepeater extends Repeater {
+
+  /** Set the name of the AppendRepeater. */
+  public AppendRepeater() {
+    // TODO: set the name of this repeater
+  }
+
+  /** Use StringBuilder to compose a String with "repeats" copies of a
+   *  specified "character". */
+  public String repeat(char character, int repeats) {
+    // TODO: add the implementation of this method for repeat2
+    return null;
+  }
+
+}
